f115fa1983cd82dea2a8c7a5ed5ef3cb3d1cb17c
Senzso <110294817+Senzso@users.noreply.github.com>
2025-02-04 00:46:37 +0100

    Update Room.tsx

--- components/Room.tsx
+++ components/Room.tsx
@@ -8,7 +8,7 @@ interface RoomProps {
 export default function Room({ items }: RoomProps) {
   return (
     <div className="relative w-full h-[600px]">
-      <Image src="/images/Room-estructure.png" alt="Room" layout="fill" objectFit="contain" />
+      <Image src="/Room-estructure.png" alt="Room" layout="fill" objectFit="contain" />
       {items.map((item, index) => (
         <div key={index} style={{ position: 'absolute', left: item.position.x, top: item.position.y }}>
           <Image src={item.image} alt={item.type} width={100} height={100} />
