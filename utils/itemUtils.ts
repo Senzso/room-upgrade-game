b9c3bd3fff8812456ebaa76f4143f41059a5677f
Senzso <110294817+Senzso@users.noreply.github.com>
2025-02-04 00:43:05 +0100

    Update itemUtils.ts

--- utils/itemUtils.ts
+++ utils/itemUtils.ts
@@ -7,19 +7,48 @@ export const initialItems: RoomItem[] = [
   // Add more initial items here
 ]
 
-const itemUpgrades: Record<ItemType, string[]> = {
-  bed: ['https://i.postimg.cc/P5FXXG7d/bed-1-left.png', 'https://i.postimg.cc/mrzbhXxC/bed-2-left.png'],
-  chair: ['https://i.postimg.cc/FzZvwwc5/Chair-1-left.png', 'https://i.postimg.cc/MK6q1K2W/chair-2-left.png'],
-  desk: ['https://i.postimg.cc/KjJX0BSm/Desk-1-left.png'],
-  lamp: ['https://i.postimg.cc/y6Pz64J2/Lamp.png'],
-  mirror: ['https://i.postimg.cc/MK32R7mD/Mirror-1-left.png', 'https://i.postimg.cc/sgBFJMnR/mirror-2-left.png'],
-  shelving: ['https://i.postimg.cc/1XR2VXg2/shelving-1-left.png', 'https://i.postimg.cc/C1s9KkKb/shelving-2-left.png', 'https://i.postimg.cc/RVF2M3xn/shelving-3-left.png'],
-  tv: ['https://i.postimg.cc/GmTNyZ2L/tv-left-1.png'],
-  closet: ['https://i.postimg.cc/VkTwyqS6/Closet-1-left.png', 'https://i.postimg.cc/3wvT3kpZ/closet-2-left.png'],
-  couch: ['https://i.postimg.cc/761rTjKG/couch-1-left.png'],
-  books: ['https://i.postimg.cc/wvDgQ1jB/Books-1-only-right.png']
+export const itemUpgrades: Record<ItemType, { left: string[]; right: string[] }> = {
+  bed: {
+    left: ["/images/bed-1-left.png", "/images/bed-2-left.png"],
+    right: ["/images/bed-1-right.png", "/images/bed-2-right.png"],
+  },
+  chair: {
+    left: ["/images/Chair-1-left.png", "/images/chair-2-left.png"],
+    right: ["/images/Chair-1-right.png", "/images/chair-2-right.png"],
+  },
+  desk: {
+    left: ["/images/Desk-1-left.png"],
+    right: ["/images/Desk-1-right.png"],
+  },
+  lamp: {
+    left: ["/images/Lamp.png"],
+    right: ["/images/Lamp.png"],
+  },
+  mirror: {
+    left: ["/images/Mirror-1-left.png", "/images/mirror-2-left.png"],
+    right: ["/images/Mirror-1-right.png", "/images/mirror-2-right.png"],
+  },
+  shelving: {
+    left: ["/images/shelving-1-left.png", "/images/shelving-2-left.png", "/images/shelving-3-left.png"],
+    right: ["/images/shelving-1-right.png", "/images/shelving-2-right.png", "/images/shelving-3-right.png"],
+  },
+  tv: {
+    left: ["/images/tv-left-1.png"],
+    right: ["/images/tv-right-1.png"],
+  },
+  closet: {
+    left: ["/images/Closet-1-left.png", "/images/closet-2-left.png"],
+    right: ["/images/Closet-1-right.png", "/images/closet-2-right.png"],
+  },
+  couch: {
+    left: ["/images/couch-1-left.png"],
+    right: ["/images/couch-1-right.png"],
+  },
+  books: {
+    left: ["/images/Books-1-only-right.png"],
+    right: ["/images/Books-1-only-right.png"],
+  },
 }
-
 export function upgradeItem(items: RoomItem[], itemType: ItemType): RoomItem[] | null {
   const itemIndex = items.findIndex(item => item.type === itemType)
   if (itemIndex === -1) return null
